f0d8b9687d2475a74b5ea21c372399cb4c50f077
Yuanxin Pan <yxpan.im@gmail.com>
2023-12-01 15:32:45 +0100

    Fix output bug of rnx2rtkp

--- app/rnx2rtkp/rnx2rtkp.c
+++ app/rnx2rtkp/rnx2rtkp.c
@@ -77,7 +77,7 @@ extern int showmsg(char *format, ...)
 {
     va_list arg;
     va_start(arg,format); vfprintf(stderr,format,arg); va_end(arg);
-    fprintf(stderr,"\n");
+    fprintf(stderr,"\r");
     return 0;
 }
 extern void settspan(gtime_t ts, gtime_t te) {}
